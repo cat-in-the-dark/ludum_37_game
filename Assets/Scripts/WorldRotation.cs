4aeb6ec2be93c411c67a5ac8ecf433dd1fc98a43
Ilya Siganov <ilya.blan4@gmail.com>
2016-12-11 13:14:34 +0600

    add level 1

--- Assets/Scripts/WorldRotation.cs
+++ Assets/Scripts/WorldRotation.cs
@@ -8,7 +8,7 @@ public class WorldRotation : MonoBehaviour {
 	IDictionary<string, GameObject> rooms = new Dictionary<string, GameObject>();
 	float currentRotation;
 	bool rotating = false;
-	Vector2[] roomGroup = null;
+	int[] roomGroup = null;
 	Vector3 axis = Vector3.zero;
 
 	GameObject roomGroupGO;
@@ -27,13 +27,13 @@ public class WorldRotation : MonoBehaviour {
 		// ==== X ====
 		if (Input.GetKeyDown (KeyCode.K)) {
 			StartRotation (
-				roomGroup:new Vector2[]{ new Vector2(1,1), new Vector2(1,2), new Vector2(1,3), new Vector2(1,4) }, 
+				roomGroup:new int[]{1,2,3,4}, 
 				axis: Vector3.down);
 		}
 
 		if (Input.GetKeyDown (KeyCode.J)) {
 			StartRotation (
-				roomGroup: new Vector2[]{ new Vector2(1,1), new Vector2(1,2), new Vector2(1,3), new Vector2(1,4) }, 
+				roomGroup:new int[]{1,2,3,4}, 
 				axis: Vector3.up);
 		}
 
@@ -41,26 +41,26 @@ public class WorldRotation : MonoBehaviour {
 		// ==== Y ====
 		if (Input.GetKeyDown (KeyCode.U)) {
 			StartRotation (
-				roomGroup:new Vector2[]{ new Vector2(1,1), new Vector2(1,2), new Vector2(2,1), new Vector2(2,2) }, 
+				roomGroup:new int[]{2,3,6,7}, 
 				axis: Vector3.back);
 		}
 
 		if (Input.GetKeyDown (KeyCode.I)) {
 			StartRotation (
-				roomGroup: new Vector2[]{ new Vector2(1,1), new Vector2(1,2), new Vector2(2,1), new Vector2(2,2) }, 
+				roomGroup:new int[]{2,3,6,7},  
 				axis: Vector3.forward);
 		}
 
 		// ==== Z ====
 		if (Input.GetKeyDown (KeyCode.N)) {
 			StartRotation (
-				roomGroup:new Vector2[]{ new Vector2(1,2), new Vector2(1,3), new Vector2(2,2), new Vector2(2,3) }, 
+				roomGroup:new int[]{1,2,5,6}, 
 				axis: Vector3.left);
 		}
 
 		if (Input.GetKeyDown (KeyCode.M)) {
 			StartRotation (
-				roomGroup:new Vector2[]{ new Vector2(1,2), new Vector2(1,3), new Vector2(2,2), new Vector2(2,3) }, 
+				roomGroup:new int[]{1,2,5,6}, 
 				axis: Vector3.right);
 		}
 
@@ -70,7 +70,7 @@ public class WorldRotation : MonoBehaviour {
 	}
 
 
-	void StartRotation(Vector2[] roomGroup, Vector3 axis) {
+	void StartRotation(int[] roomGroup, Vector3 axis) {
 		if (!this.rotating) {
 			this.rotating = true;
 			this.currentRotation = 0f;
@@ -83,7 +83,7 @@ public class WorldRotation : MonoBehaviour {
 		}
 	}
 
-	void Rotate(Vector2[] roomGroupIds, Vector3 axis) {
+	void Rotate(int[] roomGroupIds, Vector3 axis) {
 		if (roomGroupIds == null) return;
 		if (Mathf.Abs (currentRotation) >= 90.0f) {
 			roomGroupGO.transform.eulerAngles = axis * 90.0f;
@@ -97,7 +97,7 @@ public class WorldRotation : MonoBehaviour {
 		var roomGroup = new List<GameObject>();
 		var center = Vector3.zero;
 		foreach (var id in roomGroupIds) {
-			var name = string.Format ("Cube_{0}_{1}", id.x, id.y);
+			var name = string.Format ("Cube{0}", id);
 			GameObject room = rooms [name];
 			roomGroup.Add (room);
 			center.x += room.transform.position.x;
