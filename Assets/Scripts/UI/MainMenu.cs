c246961e6cdf941b78ebca08fe045d3ac29c8eb9
Ilya Siganov <ilya.blan4@gmail.com>
2016-12-11 11:30:16 +0600

    add main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void StartGame() {
+		Debug.Log ("Game Started");
+		SceneManager.LoadScene (1, LoadSceneMode.Single);
+	}
+}
