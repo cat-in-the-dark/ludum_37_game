85aa2c5340ba9126b5b9dbfbf009cef2080aca90
Ilya Siganov <ilya.blan4@gmail.com>
2016-12-11 17:18:55 +0600

    add background sound

---
+++ Assets/Scripts/UI/AudioController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AudioController : MonoBehaviour {
+	public static AudioController instance;
+
+	public AudioSource background;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance != null && instance != this) {
+			Destroy (this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+	void OnLevelWasLoaded(int level) {
+		if (level == 0) {
+			background.Stop ();
+		} else {
+			if (!background.isPlaying) {
+				background.Play ();
+			}
+		}
+	}
+}
